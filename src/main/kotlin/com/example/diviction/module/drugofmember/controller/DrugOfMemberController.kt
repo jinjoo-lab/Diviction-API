f107bc112a223afe88a1c10a4eb84f0d25e218af
WooJoongKim <rlandwnd555@naver.com>
2023-03-03 00:57:29 +0900

    feat : 중독자 약물 컨트롤러 개발

---
+++ src/main/kotlin/com/example/diviction/module/drugofmember/controller/DrugOfMemberController.kt
@@ -0,0 +1,55 @@
+package com.example.diviction.module.drugofmember.controller
+
+import com.example.diviction.module.drugofmember.dto.DrugOfMemberSaveRequest
+import com.example.diviction.module.drugofmember.entity.DrugOfMember
+import com.example.diviction.module.drugofmember.service.DrugOfMemberService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/drugofmember")
+class DrugOfMemberController (
+        private val drugOfMemberService: DrugOfMemberService
+        ){
+
+    @GetMapping("/list")
+    fun getDrugOfMemberList() : List<DrugOfMember>
+    {
+        return drugOfMemberService.getAllList()
+    }
+
+    @GetMapping("/delete/{id}")
+    fun deleteDrugOfMember(@PathVariable id : Long)
+    {
+        drugOfMemberService.deleteDrugOfMember(id)
+    }
+
+    @PostMapping("/save")
+    fun saveDrugOfMember(
+            @RequestBody drugOfMemberSaveRequest: DrugOfMemberSaveRequest
+    ) {
+        drugOfMemberService.saveDrugOfMember(drugOfMemberSaveRequest.member, drugOfMemberSaveRequest.drug)
+    }
+
+    @GetMapping("/get/bymember/{memberId}")
+    fun getDrugOfMemberByMemberId(@PathVariable memberId : Long) : List<DrugOfMember>
+    {
+        return drugOfMemberService.getListByMemberId(memberId)
+    }
+
+    @GetMapping("/get/bydrug/{drugId}")
+    fun getDrugOfMemberByDrugId(@PathVariable drugId : Long) : List<DrugOfMember>
+    {
+        return drugOfMemberService.getListByDrugId(drugId)
+    }
+
+    @GetMapping("/get/{id}")
+    fun getDrugOfMemberById(@PathVariable id : Long) : DrugOfMember
+    {
+        return drugOfMemberService.getDrugOfMember(id)
+    }
+}
