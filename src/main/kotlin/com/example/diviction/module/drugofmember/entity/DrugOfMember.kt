a54d2bc6b9f4dc9c002fff76f7c4c8c2429cecdb
WooJoongKim <rlandwnd555@naver.com>
2023-03-07 23:01:12 +0900

    fix : 회원 및 약물 엔티티 생성자로 이동

--- src/main/kotlin/com/example/diviction/module/drugofmember/entity/DrugOfMember.kt
+++ src/main/kotlin/com/example/diviction/module/drugofmember/entity/DrugOfMember.kt
@@ -2,8 +2,6 @@ package com.example.diviction.module.drugofmember.entity
 
 import com.example.diviction.module.account.entity.Member
 import com.example.diviction.module.drug.entity.Drug
-import lombok.Getter
-import lombok.Setter
 import lombok.ToString
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
@@ -13,18 +11,18 @@ import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
 
 @Entity
-@Setter
 @ToString
-class DrugOfMember {
+class DrugOfMember(
+        @ManyToOne
+        @JoinColumn(name = "member_id")
+        var member: Member? = null,
+
+        @ManyToOne
+        @JoinColumn(name = "product_id")
+        var drug: Drug? = null
+)
+{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null
-
-    @ManyToOne
-    @JoinColumn(name = "member_id")
-    var member: Member? = null
-
-    @ManyToOne
-    @JoinColumn(name = "product_id")
-    var drug: Drug? = null
 }
