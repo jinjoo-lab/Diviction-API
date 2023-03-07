e8c48685d9bc96c779ed82c74ab94ce80616509b
WooJoongKim <rlandwnd555@naver.com>
2023-03-07 23:04:01 +0900

    fix : 저장 시 회원과 약물의 id로 해당객체를 직접 검색해서 추가해 저장하도록 수직 수정 및 응답 시 MemberOfDrug 리스트에서 각 회원 혹은 약물 정보만 리스트 형식으로 가공하여 반환하도록 로직 수정

--- src/main/kotlin/com/example/diviction/module/drugofmember/service/DrugOfMemberService.kt
+++ src/main/kotlin/com/example/diviction/module/drugofmember/service/DrugOfMemberService.kt
@@ -1,39 +1,55 @@
 package com.example.diviction.module.drugofmember.service
 
 import com.example.diviction.module.account.entity.Member
+import com.example.diviction.module.account.repository.MemberRepository
 import com.example.diviction.module.drug.entity.Drug
+import com.example.diviction.module.drug.repository.DrugRepository
+import com.example.diviction.module.drugofmember.dto.DrugOfMemberResponse
+import com.example.diviction.module.drugofmember.dto.DrugOfMemberSaveRequest
+import com.example.diviction.module.drugofmember.dto.MemberOfDrugResponse
 import com.example.diviction.module.drugofmember.entity.DrugOfMember
 import com.example.diviction.module.drugofmember.repository.DrugOfMemberRepository
 import org.springframework.stereotype.Service
 
 @Service
 class DrugOfMemberService (
-        private val drugOfMemberRepository: DrugOfMemberRepository
+        private val drugOfMemberRepository: DrugOfMemberRepository,
+        private val memberRepository: MemberRepository,
+        private val drugRepository: DrugRepository
         ){
     fun getAllList() : List<DrugOfMember> {
         return drugOfMemberRepository.findAll()
     }
 
-    fun getListByMemberId(memberId : Long) : List<DrugOfMember> {
-        return drugOfMemberRepository.findByMemberId(memberId)
+    fun getListByMemberId(memberId : Long) : DrugOfMemberResponse {
+        val drugOfMemberList = drugOfMemberRepository.findByMemberId(memberId)
+        val result = ArrayList<Drug>()
+        for (drugOfMember in drugOfMemberList) {
+            result.add(drugOfMember.drug!!)
+        }
+        return DrugOfMemberResponse(result)
     }
 
-    fun getListByDrugId(drugId : Long) : List<DrugOfMember> {
-        return drugOfMemberRepository.findByDrugId(drugId)
+    fun getListByDrugId(drugId : Long) : MemberOfDrugResponse {
+        val memberOfDrugList = drugOfMemberRepository.findByDrugId(drugId)
+        val result = ArrayList<Member>()
+        for (drugOfMember in memberOfDrugList) {
+            result.add(drugOfMember.member!!)
+        }
+        return MemberOfDrugResponse(result)
     }
 
     fun deleteDrugOfMember(id : Long) {
         drugOfMemberRepository.deleteById(id)
     }
 
-    fun saveDrugOfMember(member: Member, drug: Drug) {
-        if (checkDrugOfMemberDuplicate(member.id!!, drug.id!!)) {
+    fun saveDrugOfMember(drugOfMemberSaveRequest: DrugOfMemberSaveRequest) {
+        if (checkDrugOfMemberDuplicate(drugOfMemberSaveRequest.memberId!!, drugOfMemberSaveRequest.drugId!!)) {
             throw RuntimeException("이미 존재하는 데이터입니다.")
         }
-
-        var drugOfmember = DrugOfMember()
-        drugOfmember.member = member
-        drugOfmember.drug = drug
+        val member = memberRepository.findById(drugOfMemberSaveRequest.memberId!!).get()
+        val drug = drugRepository.findById(drugOfMemberSaveRequest.drugId!!).get()
+        val drugOfmember = DrugOfMember(member, drug)
         drugOfMemberRepository.save(drugOfmember)
     }
 
