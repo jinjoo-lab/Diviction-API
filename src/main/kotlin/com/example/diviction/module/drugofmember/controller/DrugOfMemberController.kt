e8ea932717bdefd98340b9621f54a4dde7b53590
WooJoongKim <rlandwnd555@naver.com>
2023-03-07 23:01:59 +0900

    fix : 저장및 응답 로직 수정

--- src/main/kotlin/com/example/diviction/module/drugofmember/controller/DrugOfMemberController.kt
+++ src/main/kotlin/com/example/diviction/module/drugofmember/controller/DrugOfMemberController.kt
@@ -1,6 +1,8 @@
 package com.example.diviction.module.drugofmember.controller
 
+import com.example.diviction.module.drugofmember.dto.DrugOfMemberResponse
 import com.example.diviction.module.drugofmember.dto.DrugOfMemberSaveRequest
+import com.example.diviction.module.drugofmember.dto.MemberOfDrugResponse
 import com.example.diviction.module.drugofmember.entity.DrugOfMember
 import com.example.diviction.module.drugofmember.service.DrugOfMemberService
 import org.springframework.web.bind.annotation.GetMapping
@@ -32,17 +34,17 @@ class DrugOfMemberController (
     fun saveDrugOfMember(
             @RequestBody drugOfMemberSaveRequest: DrugOfMemberSaveRequest
     ) {
-        drugOfMemberService.saveDrugOfMember(drugOfMemberSaveRequest.member, drugOfMemberSaveRequest.drug)
+        drugOfMemberService.saveDrugOfMember(drugOfMemberSaveRequest)
     }
 
     @GetMapping("/get/bymember/{memberId}")
-    fun getDrugOfMemberByMemberId(@PathVariable memberId : Long) : List<DrugOfMember>
+    fun getDrugOfMemberByMemberId(@PathVariable memberId : Long) : DrugOfMemberResponse
     {
         return drugOfMemberService.getListByMemberId(memberId)
     }
 
     @GetMapping("/get/bydrug/{drugId}")
-    fun getDrugOfMemberByDrugId(@PathVariable drugId : Long) : List<DrugOfMember>
+    fun getDrugOfMemberByDrugId(@PathVariable drugId : Long) : MemberOfDrugResponse
     {
         return drugOfMemberService.getListByDrugId(drugId)
     }
