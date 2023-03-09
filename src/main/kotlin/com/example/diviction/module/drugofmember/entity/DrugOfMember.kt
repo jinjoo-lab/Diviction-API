c92a71c3a4c9596af1c668fab849c602dc0f0a77
WooJoongKim <rlandwnd555@naver.com>
2023-03-10 00:38:04 +0900

    fix : 테이블 이름 수정

--- src/main/kotlin/com/example/diviction/module/drugofmember/entity/DrugOfMember.kt
+++ src/main/kotlin/com/example/diviction/module/drugofmember/entity/DrugOfMember.kt
@@ -10,7 +10,7 @@ import javax.persistence.Id
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
 
-@Entity
+@Entity(name = "drug_member")
 @ToString
 class DrugOfMember(
         @ManyToOne
