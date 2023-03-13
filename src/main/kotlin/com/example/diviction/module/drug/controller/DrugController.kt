7f8383c63c16e22950b0a89ca72b575ef777ed0b
WooJoongKim <rlandwnd555@naver.com>
2023-03-13 23:12:24 +0900

    comment : API 설명 추가

--- src/main/kotlin/com/example/diviction/module/drug/controller/DrugController.kt
+++ src/main/kotlin/com/example/diviction/module/drug/controller/DrugController.kt
@@ -3,6 +3,7 @@ package com.example.diviction.module.drug.controller
 import com.example.diviction.module.drug.dto.DrugRegistrationRequest
 import com.example.diviction.module.drug.entity.Drug
 import com.example.diviction.module.drug.service.DrugService
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -11,18 +12,21 @@ class DrugController (
         private val drugService: DrugService
         ){
 
+    @Operation(description = "약물 등록")
     @PostMapping("/save")
     fun saveDrug(@RequestBody drugRegistrationRequest : DrugRegistrationRequest)
     {
         drugService.saveDrug(drugRegistrationRequest)
     }
 
+    @Operation(description = "약물 리스트 조회 (등록된 모든 약물 조회)")
     @GetMapping("/list")
     fun getDrugList() : List<Drug>
     {
         return drugService.getDrugList()
     }
 
+    @Operation(description = "약물 id로 약물 정보 삭제")
     @GetMapping("/delete/{id}")
     fun deleteDrug(@PathVariable id : Long)
     {
