827fd9470681c17008b356be17df28dc1669b030
WooJoongKim <rlandwnd555@naver.com>
2023-03-21 01:00:58 +0900

    feat : GCP 스토리지 기본 URL object 추가

---
+++ src/main/kotlin/com/example/diviction/infra/gcp/CommonCodes.kt
@@ -0,0 +1,8 @@
+package com.example.diviction.infra.gcp
+
+object GCP_URLs{
+    val COUNSELOR_BASIC_IMG_URL = "https://storage.cloud.google.com/diviction/user-profile/user-basic-icon.png" // 상담사 기본 프로필 사진
+    val PATIENT_BASIC_IMG_URL = "https://storage.cloud.google.com/diviction/user-profile/user-profile_user-basic-icon.jpg"  // 환자 기본 프로필 사진
+    val STORAGE_BASE_URL = "https://storage.cloud.google.com/diviction/"
+    val USER_PROFILE_URL = STORAGE_BASE_URL + "user-profile/"
+}
